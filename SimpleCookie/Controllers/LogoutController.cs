94b414912196a341b9fcbb90e00d638236fb5bdc
t.takigami <takigami-t@ise-ics.co.jp>
2020-07-29 15:55:30 +0900

    add SimpleCookie auth

---
+++ SimpleCookie/Controllers/LogoutController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCookie.Controllers
+{
+    public class LogoutController : Controller
+    {
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            await HttpContext.SignOutAsync();
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ SimpleCookie/README.md
@@ -0,0 +1,3 @@
+## 参照
+* https://github.com/dotnet/aspnetcore/tree/release/3.1/src/Security/samples/Cookies
+* https://docs.microsoft.com/ja-jp/aspnet/core/security/authentication/cookie?view=aspnetcore-3.1
